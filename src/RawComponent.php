7dbe89af988c1c964b4ce630f9d462ed2da8a0e5
Erick de Azevedo Lima <ericklima.comp@gmail.com>
2026-06-24 11:59:58 -0300

    [fix] Update docblock for rawComponentStartingWith method to include defaultAttributes parameter

--- src/RawComponent.php
+++ src/RawComponent.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \ErickComp\RawBladeComponents\RawComponentsManager rawComponent(string $tag, string $openingCode, string $closingCode, ?string $selfClosingCode = null, array $defaultAttributes = [])
- * @method static \ErickComp\RawBladeComponents\rawComponentStartingWith rawComponentStartingWith(string $tag, string $openingCode, string $closingCode, ?string $selfClosingCode = null)
+ * @method static \ErickComp\RawBladeComponents\RawComponentsManager rawComponentStartingWith(string $tag, string $openingCode, string $closingCode, ?string $selfClosingCode = null, array $defaultAttributes = [])
  * @method static string compileRawBladeComponents(string $templateStr)
  */
 class RawComponent extends Facade
